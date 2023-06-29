c4476d6701e41cd3959deb2a22ae914098b2108d
Nivedan Liye <115181358+Nivedan99@users.noreply.github.com>
2023-06-29 23:44:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-const nav1 = document.querySelector('.nav-content');
+const nav = document.querySelector('.nav-content');
 const addnav = document.querySelector('.icon');
 const removenav = document.querySelector('.remove-nav');    
 
 addnav.addEventListener('click',function(){
-    nav1.classList.add("open");
-    addnav.classList.add('none');
+    nav.classList.add("open-nav");
 })
 removenav.addEventListener('click',function(){
-    nav1.classList.remove("open");
-    addnav.classList.remove('none');
+    nav.classList.remove("open-nav");
 })
